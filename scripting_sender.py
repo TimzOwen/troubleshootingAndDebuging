5a391d7ffa5166d7bd6f970916f206730022094c
Timz Owen <ktimothy@kabarak.ac.ke>
2020-12-21 07:21:53 +0300

    Create scripting_sender.py
    app keeps failing, fix the issue

---
+++ scripting_sender.py
@@ -0,0 +1,11 @@
+# Meeting email reminder
+# Fix the issue of some meetings not working
+
+meeting_info = $(zenity --forms\
+    --title 'Meeting'--text 'Reminder information'\
+    --add-calender 'Date' --add-entry 'Title' \
+    --add-entry 'Emails'\
+    2>/dev/null)
+if [[ -n "$meeting_info" ]]; then
+    python3 send_reminders.py "meeting info"
+fi
