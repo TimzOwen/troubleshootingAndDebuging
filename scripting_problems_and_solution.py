73429fd9020d7a9500f92fa09fffdb84b3436f17
Timz Owen <ktimothy@kabarak.ac.ke>
2020-12-21 07:51:40 +0300

    Update scripting_problems_and_solution.py
    Linear and binary search syntax

--- scripting_problems_and_solution.py
+++ scripting_problems_and_solution.py
@@ -143,3 +143,33 @@ print(next_date(str(today)))
 
 print(next_date("2021-01-01")) # Should return 2022-01-01
 print(next_date("2020-02-29")) # Should return 2024-02-29
+
+
+
+def linear_search(list, key):
+    """If key is in the list returns its position in the list,
+       otherwise returns -1."""
+    for i, item in enumerate(list):
+        if item == key:
+            return i
+    return -1
+
+# Binary search syntax
+def binary_search(list, key):
+    """Returns the position of key in the list if found, -1 otherwise.
+
+    List must be sorted.
+    """
+    left = 0
+    right = len(list) - 1
+    while left <= right:
+        middle = (left + right) // 2
+
+        if list[middle] == key:
+            return middle
+        if list[middle] > key:
+            right = middle - 1
+        if list[middle] < key:
+            left = middle + 1
+    return -1
+
